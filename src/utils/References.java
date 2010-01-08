91451a9d4be95f4e9fc09cc86e575429aefc0dfc
martin.sloup <martin.sloup@06659112-a2a5-11de-a590-a9c80ee327d0>
2010-01-08 17:09:53 +0000

    HGAE 0.3

--- src/utils/References.java
+++ src/utils/References.java
@@ -1,22 +1,20 @@
 /*
  * References.java
- * This file is part of HandyGeocaching.
  *
- * HandyGeocaching is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * (read more at: http://www.gnu.org/licenses/gpl.html)
+ * Created on 19. listopad 2007, 22:08
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
  */
+
 package utils;
 
-import database.Settings;
 import gps.GpsParser;
 import gps.Internal;
 import gui.Gui;
 
 /**
- * 
+ *
  * @author David Vavra
  */
 public class References
@@ -27,10 +25,10 @@ public class References
     {
     }
     
-    public static Internal getInternal(Gui ref, GpsParser ref2, Settings ref3)
+    public static Internal getInternal(Gui ref, GpsParser ref2)
     {
        if (internal == null)
-           internal = new Internal(ref, ref2, ref3);
+           internal = new Internal(ref, ref2);
        return internal;
     }
     
