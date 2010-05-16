ad97dd2a44a67a2c8c50123ad9d597473288fdca
martin.sloup <martin.sloup@06659112-a2a5-11de-a590-a9c80ee327d0>
2010-05-16 11:41:55 +0000

    Pridana podpora pro interni kompas, pokud ji zarizeni umi (potreba otestovat). Opraven mensi bug s parsovanim souradnic.

---
+++ trunk/src/gps/CompassImplementation.java
@@ -0,0 +1,41 @@
+/*
+ * CompassImplementation.java
+ * This file is part of HandyGeocaching.
+ *
+ * HandyGeocaching is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * (read more at: http://www.gnu.org/licenses/gpl.html)
+ */
+
+package gps;
+
+/**
+ * Implementace abstraktní třídy kompasu
+ * @author Arcao
+ */
+public class CompassImplementation extends Compass {
+    public float getAzimuth() {
+        try {
+            javax.microedition.location.Orientation o = javax.microedition.location.Orientation.getOrientation();
+            if (o.isOrientationMagnetic()) {
+                return o.getCompassAzimuth() + magneticDeclination;
+            } else {
+                return o.getCompassAzimuth();
+            }
+
+        } catch (Exception e) {
+            return Float.NaN;
+        }
+    }
+     public boolean isSupported() {
+
+        try {
+            javax.microedition.location.Orientation.getOrientation();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
