5538f81f3f40a93e1f5f72e6859164516e467375
martin.sloup <martin.sloup@06659112-a2a5-11de-a590-a9c80ee327d0>
2010-06-05 19:02:33 +0000

    Mensi hratky s magnetickym kompasem, pridano Permissions-Opt pro orientaci. Pridan MathUtil pro podporu nekterych neexistujicich matematickych operaci. Pridana projekce bodu do Dalsi funkce, zatim provizorni ikona. Do require permissions pridano cteni, casem se snad dostane do podepsane verze, takze jiz nebude potrebne povolovani ctecich dialogu.

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -241,53 +241,99 @@ public class Utils
      */
     static public double atan(double x)
     {
-        final double SQRT3 = 1.732050807568877294;
-        boolean signChange=false;
-        boolean Invert=false;
-        int sp=0;
+        double SQRT3 = 1.732050807568877294;
+        boolean signChange = false;
+        boolean invert = false;
+        int sp = 0;
         double x2, a;
         // check up the sign change
-        if(x<0.)
-        {
-            x=-x;
-            signChange=true;
+        if (x < 0.) {
+            x = -x;
+            signChange = true;
         }
         // check up the invertation
-        if(x>1.)
-        {
-            x=1/x;
-            Invert=true;
+        if (x > 1.) {
+            x = 1 / x;
+            invert = true;
         }
         // process shrinking the domain until x<PI/12
-        while(x>Math.PI/12)
-        {
+        while (x > Math.PI / 12) {
             sp++;
-            a=x+SQRT3;
-            a=1/a;
-            x=x*SQRT3;
-            x=x-1;
-            x=x*a;
+            a = x + SQRT3;
+            a = 1 / a;
+            x = x * SQRT3;
+            x = x - 1;
+            x = x * a;
         }
         // calculation core
-        x2=x*x;
-        a=x2+1.4087812;
-        a=0.55913709/a;
-        a=a+0.60310579;
-        a=a-(x2*0.05160454);
-        a=a*x;
+        x2 = x * x;
+        a = x2 + 1.4087812;
+        a = 0.55913709 / a;
+        a = a + 0.60310579;
+        a = a - (x2 * 0.05160454);
+        a = a * x;
         // process until sp=0
-        while(sp>0)
-        {
-            a=a+Math.PI/6;
+        while (sp > 0) {
+            a = a + Math.PI / 6;
             sp--;
         }
         // invertation took place
-        if(Invert) a=Math.PI/2-a;
+        if (invert) {
+            a = Math.PI / 2 - a;
+        }
         // sign change took place
-        if(signChange) a=-a;
+        if (signChange) {
+            a = -a;
+        }
         //
         return a;
     }
+    
+    public static double asin(double x) {
+        if (x < -1. || x > 1.) {
+            return Double.NaN;
+        }
+        if (x == -1.) {
+            return -Math.PI / 2;
+        }
+        if (x == 1) {
+            return Math.PI / 2;
+        }
+        return atan(x / Math.sqrt(1 - x * x));
+    }
+
+    /**
+     * Compute atan with right quadrant
+     * http://www.dspguru.com/comp.dsp/tricks/alg/fxdatan2.htm
+     * @param y delta y
+     * @param x delta x
+     * @return value atan2(dx, dy)
+     */
+    public static double atan2(double y, double x) {
+        // if x=y=0
+        if (y == 0. && x == 0.) {
+            return 0.;
+        }
+        // if x>0 atan(y/x)
+        if (x > 0.) {
+            return atan(y / x);
+        }
+        // if x<0 sign(y)*(pi - atan(|y/x|))
+        if (x < 0.) {
+            if (y < 0.) {
+                return -(Math.PI - atan(y / x));
+            } else {
+                return Math.PI - atan(-y / x);
+            }
+        }
+        // if x=0 y!=0 sign(y)*pi/2
+        if (y < 0.) {
+            return -Math.PI / 2.;
+        } else {
+            return Math.PI / 2.;
+        }
+    }
+
     
     /**
      * J2ME nema str_replace, zde je jeji implementace
